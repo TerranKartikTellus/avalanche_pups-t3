50d6690f4bc20e0c813d2aee7392e7f85be5013b
terrankartiktellus <terrankartiktellus@gmail.com>
2024-02-29 14:12:34 +0530

    error resolution

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import { Inter } from "@next/font/google"
+import { Inter } from "next/font/google"
 import Image from "next/image"
 import snow from "../public/img/snow.jpg"
 import logo from "../public/img/logo1.jpg"
